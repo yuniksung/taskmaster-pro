c52ca8d8f0c12842d848b07877479736aa243a43
Joshua Sung <yuniksung@gmail.com>
2020-10-18 21:15:39 -0500

    I don't know what I am doing

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,66 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+$(".list-group").on("click", "p", function() {
+var text = $(this)
+  .text()
+  .trim();
+var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
+$(".list-group").on("blur", "textarea", function(){
+var text = $(this)
+  .val()
+  .trim();
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+  tasks[status][index].text = text;
+  saveTasks();
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+  $(this).replaceWith(taskP);
+});
+
+$(".list-group").on("click", "span", function() {
+  var date = $(this)
+    .text()
+    .trim();
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  $(this).replaceWith(dateInput);
+  dateInput.trigger("focus");  
+});
+
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this)
+    .val()
+    .trim();
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  tasks[status][index].date = date;
+  saveTasks();
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+  $(this).replaceWith(taskSpan);
+});
 
 
 
@@ -93,5 +152,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
